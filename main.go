4767711f19c9847786e526e3da553268e1f81521
Samantha-Grace <sgrace732@gmail.com>
2022-04-01 09:38:30 -0600

    format and bug [byte] hah it's too early need coffee

--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 type ClientManager struct {
-	clients map[*Client]bool
-	broadcast chan []byte
-	register chan *Client
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
 	unregister chan *Client
 }
 
 type Client struct {
-	id string
+	id     string
 	socket *websocket.Conn
-	send chan [byte]
+	send   chan []byte
 }
 
 type Messenger struct {
-	Sender string `json:"sender,omitempty"`
+	Sender    string `json:"sender,omitempty"`
 	Recipient string `json:"recipient,omitempty"`
-	Content string `json:"content,omitempty"`
+	Content   string `json:"content,omitempty"`
 }
 
 type Message struct {
-	Sender string `json:"sender,omitempty"`
+	Sender    string `json:"sender,omitempty"`
 	Recipient string `json:"recipient,omitempty"`
-	Content string `json:"content,omitempty"`
+	Content   string `json:"content,omitempty"`
 }
 
 func main() {
